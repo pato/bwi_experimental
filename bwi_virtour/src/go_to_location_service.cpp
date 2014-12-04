de8ecf949ef00c8170abeec9ed79469a5d7cde64
Pato <plankenau@gmail.com>
2014-12-03 20:15:51 -0600

    added the actual actionlib call

--- bwi_virtour/src/go_to_location_service.cpp
+++ bwi_virtour/src/go_to_location_service.cpp
@@ -5,12 +5,47 @@
 
 typedef actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> Client;
 
+Client* client;
+
 using namespace std;
 
 bool goToLocation(bwi_virtour::GoToLocation::Request &req,
     bwi_virtour::GoToLocation::Response &res) {
   ROS_INFO("requesting goToLocation: %s", req.location.c_str());
-  res.result = 1;
+  ROS_INFO("im alive");
+
+  ROS_INFO("waiting for server");
+  client->waitForServer();
+  
+  ROS_INFO("creating goal");
+  bwi_kr_execution::ExecutePlanGoal goal;
+  
+  bwi_kr_execution::AspRule rule;
+  bwi_kr_execution::AspFluent fluent;
+  fluent.name = "not at";
+  
+  fluent.variables.push_back(req.location);
+ 
+  rule.body.push_back(fluent);
+  goal.aspGoal.push_back(rule);
+  
+  ROS_INFO("sending goal");
+  client->sendGoalAndWait(goal);
+  
+  if (client->getState() == actionlib::SimpleClientGoalState::ABORTED) {
+    ROS_INFO("Aborted");
+    res.result = -3;
+  } else if (client->getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+    ROS_INFO("Preempted");
+    res.result = -2;
+  } else if (client->getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    ROS_INFO("Succeeded!");
+    res.result = 1;
+  } else {
+     ROS_INFO("Terminated");
+     res.result = -1;
+  }
+    
   ROS_INFO("sending back response: [%ld]", (long int)res.result);
   return true;
 }
@@ -19,17 +54,16 @@ bool goToLocation(bwi_virtour::GoToLocation::Request &req,
 int main(int argc, char**argv) {
   ros::init(argc, argv, "go_to_location_service_node");
   ros::NodeHandle n;
-  
-  string location = "l3_414b";
+
+  client = new Client("/action_executor/execute_plan", true);
 
   ros::ServiceServer service = n.advertiseService("go_to_location", goToLocation);
   ROS_INFO("GoToLocation Service Started");
+
   ros::spin();
   ROS_INFO("Done spinning");
-  
-  ROS_INFO_STREAM("going to " << location);
-
   return 0;
+   /*
   
   Client client("/action_executor/execute_plan", true);
   client.waitForServer();
@@ -59,4 +93,6 @@ int main(int argc, char**argv) {
   }
     
   return 0;
+
+  */
 }
