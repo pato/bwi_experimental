a1b6c89a26df98a6d5e9ca29631865bb3cf0aa15
Pato <plankenau@gmail.com>
2014-12-11 17:20:59 -0600

    cleaning up

--- bwi_virtour/src/go_to_location_service.cpp
+++ bwi_virtour/src/go_to_location_service.cpp
@@ -63,36 +63,4 @@ int main(int argc, char**argv) {
   ros::spin();
   ROS_INFO("Done spinning");
   return 0;
-   /*
-  
-  Client client("/action_executor/execute_plan", true);
-  client.waitForServer();
-  
-  bwi_kr_execution::ExecutePlanGoal goal;
-  
-  bwi_kr_execution::AspRule rule;
-  bwi_kr_execution::AspFluent fluent;
-  fluent.name = "not at";
-  
-  fluent.variables.push_back(location);
- 
-  rule.body.push_back(fluent);
-  goal.aspGoal.push_back(rule);
-  
-  ROS_INFO("sending goal");
-  client.sendGoalAndWait(goal);
-  
-  if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
-    ROS_INFO("Aborted");
-  } else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
-    ROS_INFO("Preempted");
-  } else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Succeeded!");
-  } else {
-     ROS_INFO("Terminated");
-  }
-    
-  return 0;
-
-  */
 }
