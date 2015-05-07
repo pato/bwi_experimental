a4209392fdea15b4bad1d2a01a45a0a5153985a5
Pato <plankenau@gmail.com>
2015-05-07 14:49:33 -0500

    added shebang

--- bwi_virtour/broadcastTour.sh
+++ bwi_virtour/broadcastTour.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 hostname=$(hostname)
 link="http://nixons-head.csres.utexas.edu/tours"
 echo -e "Tour started on $hostname\nCheck it out at: $link" | mail -s "[bwi_virtour] Tour started on $hostname" plankenau@gmail.com
