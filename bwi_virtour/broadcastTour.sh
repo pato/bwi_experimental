9d6519938f524f8bf96111c521e731235a29ae20
Pato <plankenau@gmail.com>
2015-05-07 14:46:19 -0500

    fixed newline

--- bwi_virtour/broadcastTour.sh
+++ bwi_virtour/broadcastTour.sh
@@ -1,3 +1,3 @@
 hostname=$(hostname)
 link="http://nixons-head.csres.utexas.edu/tours"
-echo "Tour started on $hostname\nCheck it out at: $link" | mail -s "[bwi_virtour] Tour started on $hostname" plankenau@gmail.com
+echo -e "Tour started on $hostname\nCheck it out at: $link" | mail -s "[bwi_virtour] Tour started on $hostname" plankenau@gmail.com
